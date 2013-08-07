7acf54b9302fdee851e5f690eace9abc4da5853b
pislas <pabloislas@gmail.com>
2013-08-06 20:50:37 -0400

    Abm Simple de Profesiones Change-Id: I70e5da99f68daf0eae3ebbd9adbda655211075bd

--- Implementacion/prh/module/Org/src/Org/Controller/ProfesionController.php
+++ Implementacion/prh/module/Org/src/Org/Controller/ProfesionController.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace Org\Controller;
-use EnterpriseSolutions\Controller\BaseController as SimpleController;
-//use EnterpriseSolutions\Controller\SimpleController;
+//use EnterpriseSolutions\Controller\BaseController as SimpleController;
+use EnterpriseSolutions\Controller\SimpleController;
 
 class ProfesionController extends SimpleController
 {
-
+	public $_tableName = 'org_profesion';
 }
