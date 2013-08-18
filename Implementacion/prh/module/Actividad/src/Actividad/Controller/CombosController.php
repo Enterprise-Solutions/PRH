247066b2162416673ceb3ccb649a4d8299647adc
Guido Arce <guidoarce89@gmail.com>
2013-08-18 00:46:38 -0400

    Combos para crear y editar actividades

--- Implementacion/prh/module/Actividad/src/Actividad/Controller/CombosController.php
+++ Implementacion/prh/module/Actividad/src/Actividad/Controller/CombosController.php
@@ -34,4 +34,31 @@ class CombosController extends BaseController
         $template = $this->_crearTemplateParaListado();
         return $template($dao);
     }
+    
+    public function actActividadTipoAction()
+    {
+        $select = new Select($this->getServiceLocator()->get('Zend\Db\Adapter\Adapter'));
+        $select->_select->from('act_actividad_tipo')->columns(array('act_actividad_tipo_id', 'nombre'));
+        $dao = new Dao($select);
+        $template = $this->_crearTemplateParaListado();
+        return $template($dao);
+    }
+    
+    public function contMonedaAction()
+    {
+        $select = new Select($this->getServiceLocator()->get('Zend\Db\Adapter\Adapter'));
+        $select->_select->from('cont_moneda')->columns(array('cont_moneda_id', 'nombre'));
+        $dao = new Dao($select);
+        $template = $this->_crearTemplateParaListado();
+        return $template($dao);
+    }
+    
+    public function calAnhoFormacionAction()
+    {
+        $select = new Select($this->getServiceLocator()->get('Zend\Db\Adapter\Adapter'));
+        $select->_select->from('cal_anho_formacion')->columns(array('cal_anho_formacion_id', 'anho'));
+        $dao = new Dao($select);
+        $template = $this->_crearTemplateParaListado();
+        return $template($dao);
+    }
 }
