1b6c7fb04e0ee0f99369bf0b7412cc6f1465d0c1
Guido Arce <guidoarce89@gmail.com>
2013-12-27 16:52:18 -0300

    Cambios Get de Actividad Tipo
    Cambios correspondientes para la nueva BD

--- Implementacion/prh/module/Actividad/src/Actividad/ActividadTipo/QueryObject/Get.php
+++ Implementacion/prh/module/Actividad/src/Actividad/ActividadTipo/QueryObject/Get.php
@@ -9,13 +9,7 @@ class Get extends DbSelect
     public function _init()
     {
         $this->_select
-             ->from(array('aat' => 'act_actividad_tipo'))
-             ->columns(array('act_actividad_tipo_id', 'nombre', 'codigo', 'subtitulo', 'atelier',
-                 'asistencia', 'duracion', 'fecha_baja', 'fecha_alta'))
-             
-             ->join(array('an' => 'act_nivel'), 'aat.act_nivel_id = an.act_nivel_id', array('act_nivel_id', 'nivel' => 'nombre'))
-             ->join(array('am' => 'act_modalidad'), 'aat.act_modalidad_id = am.act_modalidad_id', array('act_modalidad_id', 'modalidad' => 'nombre'))
-             ->join(array('ac' => 'act_criterio'), 'aat.act_criterio_id = ac.act_criterio_id', array('act_criterio_id', 'criterio' => 'codigo'));
+             ->from(array('aat' => 'act_actividad_tipo'));
     }
     
     public function addSearchById($id = null)
