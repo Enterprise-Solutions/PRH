3cfc7036fcd0b8ba9c06f3e0c5b3365405b9228c
Guido Arce <guidoarce89@gmail.com>
2013-12-29 16:02:12 -0300

    Listado y Visualizacion de Actividades
    Se implemento el index y el get de actividades. Hay que confirmar los
    datos enviados

--- Implementacion/prh/module/Actividad/src/Actividad/Actividad/QueryObject/Select.php
+++ Implementacion/prh/module/Actividad/src/Actividad/Actividad/QueryObject/Select.php
@@ -11,11 +11,11 @@ class Select extends DbSelect
         $this->_select
              ->from(array('aa' => 'act_actividad'))
              ->columns(array('act_actividad_id', 'fecha_inicio', 'fecha_fin',
-                 'nombre_identificador', 'duracion', 'estado', 'monto', 'tipo',
-                 'observaciones', 'nro_personas'))
+                 'titulo', 'duracion', 'monto_referencial'))
              
-             ->join(array('act' => 'act_actividad_tipo'), 'aa.act_actividad_tipo_id = act.act_actividad_tipo_id', array('actividad_tipo' => 'nombre'))
+             ->join(array('act' => 'act_actividad_tipo'), 'aa.act_actividad_tipo_id = act.act_actividad_tipo_id', array('actividad_tipo' => 'titulo'))
              ->join(array('cm' => 'cont_moneda'), 'aa.cont_moneda_id = cm.cont_moneda_id', array('moneda' => 'nombre'))
-             ->join(array('caf' => 'cal_anho_formacion'), 'aa.cal_anho_formacion_id = caf.cal_anho_formacion_id', array('anho_formacion' => 'anho'));
+             ->join(array('caf' => 'cal_anho_formacion'), 'aa.cal_anho_formacion_id = caf.cal_anho_formacion_id', array('anho_formacion' => 'anho'))
+             ->join(array('ac' => 'act_ciclo'), 'aa.act_ciclo_id = ac.act_ciclo_id', array('ciclo' => 'nombre'));
 	}
 }
