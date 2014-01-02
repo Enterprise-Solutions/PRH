f9d25b9c990e41e9e00c0e17eb781b401455fa59
Guido Arce <guidoarce89@gmail.com>
2014-01-02 17:24:08 -0300

    Actualizacion Index y Get de Actividades
    Se actualizaron el listado y el get de actividad

--- Implementacion/prh/module/Actividad/src/Actividad/Actividad/QueryObject/Get.php
+++ Implementacion/prh/module/Actividad/src/Actividad/Actividad/QueryObject/Get.php
@@ -3,6 +3,7 @@
 namespace Actividad\Actividad\QueryObject;
 
 use EnterpriseSolutions\Db\Select as DbSelect;
+use Zend\Db\Sql\Expression;
 
 class Get extends DbSelect
 {
@@ -12,7 +13,7 @@ class Get extends DbSelect
              ->from(array('aa' => 'act_actividad'))
              
              ->join(array('aat' => 'act_actividad_tipo'), 'aa.act_actividad_tipo_id = aat.act_actividad_tipo_id', array('act_actividad_tipo_id', 'actividad_tipo' => 'titulo'))
-             ->join(array('cm' => 'cont_moneda'), 'aa.cont_moneda_id = cm.cont_moneda_id', array('cont_moneda_id', 'moneda' => 'nombre'))
+             ->join(array('cm' => 'cont_moneda'), 'aa.cont_moneda_id = cm.cont_moneda_id', array('cont_moneda_id', 'moneda' => 'nombre', 'precio' => new Expression("cm.simbolo || ' ' || aa.monto_referencial	")))
              ->join(array('caf' => 'cal_anho_formacion'), 'aa.cal_anho_formacion_id = caf.cal_anho_formacion_id', array('cal_anho_formacion_id', 'anho_formacion' => 'anho'))
         	 ->join(array('ac' => 'act_ciclo'), 'aa.act_ciclo_id = ac.act_ciclo_id', array('act_ciclo_id', 'ciclo' => 'nombre'));
     }
--- Implementacion/prh/module/Actividad/src/Actividad/Actividad/QueryObject/Select.php
+++ Implementacion/prh/module/Actividad/src/Actividad/Actividad/QueryObject/Select.php
@@ -11,9 +11,9 @@ class Select extends DbSelect
         $this->_select
              ->from(array('aa' => 'act_actividad'))
              ->columns(array('act_actividad_id', 'fecha_inicio', 'fecha_fin',
-                 'titulo', 'duracion', 'monto_referencial'))
+                 'titulo', 'duracion', 'monto_referencial', 'modalidad_act'))
              
-             ->join(array('act' => 'act_actividad_tipo'), 'aa.act_actividad_tipo_id = act.act_actividad_tipo_id', array('actividad_tipo' => 'titulo'))
+             ->join(array('act' => 'act_actividad_tipo'), 'aa.act_actividad_tipo_id = act.act_actividad_tipo_id', array('actividad_tipo' => 'titulo', 'modalidad'))
              ->join(array('cm' => 'cont_moneda'), 'aa.cont_moneda_id = cm.cont_moneda_id', array('moneda' => 'nombre'))
              ->join(array('caf' => 'cal_anho_formacion'), 'aa.cal_anho_formacion_id = caf.cal_anho_formacion_id', array('anho_formacion' => 'anho'))
              ->join(array('ac' => 'act_ciclo'), 'aa.act_ciclo_id = ac.act_ciclo_id', array('ciclo' => 'nombre'));
