b2b4eef45f22fa350375ebb343683d8a4f280c56
Guido Arce <guidoarce89@gmail.com>
2014-01-03 12:25:37 -0300

    Se agrego modalidad al get de actividad

--- Implementacion/prh/module/Actividad/src/Actividad/Actividad/QueryObject/Get.php
+++ Implementacion/prh/module/Actividad/src/Actividad/Actividad/QueryObject/Get.php
@@ -13,7 +13,7 @@ class Get extends DbSelect
         $this->_select
              ->from(array('aa' => 'act_actividad'))
              
-             ->join(array('aat' => 'act_actividad_tipo'), 'aa.act_actividad_tipo_id = aat.act_actividad_tipo_id', array('act_actividad_tipo_id', 'actividad_tipo' => 'titulo'))
+             ->join(array('aat' => 'act_actividad_tipo'), 'aa.act_actividad_tipo_id = aat.act_actividad_tipo_id', array('act_actividad_tipo_id', 'actividad_tipo' => 'titulo', 'modalidad'))
              ->join(array('cm' => 'cont_moneda'), 'aa.cont_moneda_id = cm.cont_moneda_id', array('cont_moneda_id', 'moneda' => 'nombre', 'precio' => new Expression("cm.simbolo || ' ' || aa.monto_referencial	")))
              ->join(array('caf' => 'cal_anho_formacion'), 'aa.cal_anho_formacion_id = caf.cal_anho_formacion_id', array('cal_anho_formacion_id', 'anho_formacion' => 'anho'))
         	 ->join(array('ac' => 'act_ciclo'), 'aa.act_ciclo_id = ac.act_ciclo_id', array('act_ciclo_id', 'ciclo' => 'nombre'))
