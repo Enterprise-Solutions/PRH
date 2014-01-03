af34c5b6deb0a0a50870e8093ca51eaf6d0e67ea
Guido Arce <guidoarce89@gmail.com>
2014-01-03 11:49:45 -0300

    Se agrego el formador al get de actividad

--- Implementacion/prh/module/Actividad/src/Actividad/Actividad/QueryObject/Get.php
+++ Implementacion/prh/module/Actividad/src/Actividad/Actividad/QueryObject/Get.php
@@ -4,6 +4,7 @@ namespace Actividad\Actividad\QueryObject;
 
 use EnterpriseSolutions\Db\Select as DbSelect;
 use Zend\Db\Sql\Expression;
+use Zend\Db\Sql\Select as ZfSelect;
 
 class Get extends DbSelect
 {
@@ -15,7 +16,10 @@ class Get extends DbSelect
              ->join(array('aat' => 'act_actividad_tipo'), 'aa.act_actividad_tipo_id = aat.act_actividad_tipo_id', array('act_actividad_tipo_id', 'actividad_tipo' => 'titulo'))
              ->join(array('cm' => 'cont_moneda'), 'aa.cont_moneda_id = cm.cont_moneda_id', array('cont_moneda_id', 'moneda' => 'nombre', 'precio' => new Expression("cm.simbolo || ' ' || aa.monto_referencial	")))
              ->join(array('caf' => 'cal_anho_formacion'), 'aa.cal_anho_formacion_id = caf.cal_anho_formacion_id', array('cal_anho_formacion_id', 'anho_formacion' => 'anho'))
-        	 ->join(array('ac' => 'act_ciclo'), 'aa.act_ciclo_id = ac.act_ciclo_id', array('act_ciclo_id', 'ciclo' => 'nombre'));
+        	 ->join(array('ac' => 'act_ciclo'), 'aa.act_ciclo_id = ac.act_ciclo_id', array('act_ciclo_id', 'ciclo' => 'nombre'))
+        	 ->join(array('aaf' => 'act_actividad_formadores'), 'aa.act_actividad_id = aaf.act_actividad_id', array(), ZfSelect::JOIN_LEFT)
+        	 ->join(array('opr' => 'org_parte_rol'), 'aaf.org_parte_rol_id = opr.org_parte_rol_id', array(), ZfSelect::JOIN_LEFT)
+        	 ->join(array('op' => 'org_parte'), 'opr.org_parte_id = op.org_parte_id', array('formador' => new Expression("nombre_persona || ' ' || apellido_persona")), ZfSelect::JOIN_LEFT);
     }
     
     public function addSearchById($id = null)
