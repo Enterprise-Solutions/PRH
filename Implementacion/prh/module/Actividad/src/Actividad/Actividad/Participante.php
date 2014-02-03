a02a193114eb2dff5c69d73781daf3538d463703
Guido Arce <guidoarce89@gmail.com>
2014-02-03 14:03:16 -0300

    Post Actividades Relacion Ayuda
    Se implementó la creación de actividades de relacion de ayuda.

--- Implementacion/prh/module/Actividad/src/Actividad/Actividad/Participante.php
+++ Implementacion/prh/module/Actividad/src/Actividad/Actividad/Participante.php
@@ -32,6 +32,60 @@ class Participante
      */
     protected $participante;
     
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $cont_moneda_id;
+    
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $act_actividad_formadores_id;
+    
+    /**
+     * @ORM\Column(type="string")
+     * @ORM\Column(length=80)
+     */
+    protected $identificador_participante;
+    
+    /**
+     * @ORM\Column(type="string")
+     * @ORM\Column(length=100)
+     */
+    protected $sobrenombre;
+    
+    /**
+     * @ORM\Column(type="string")
+     * @ORM\Column(length=250)
+     */
+    protected $descripcion;
+    
+    /**
+     * @ORM\Column(type="string")
+     * @ORM\Column(length=250)
+     */
+    protected $otra_info;
+    
+    /**
+     * @Orm\Column(type="float")
+     */
+    protected $monto_participante;
+    
+    /**
+     * @Orm\Column(type="boolean")
+     */
+    protected $se_imprimio_certificado;
+    
+    /**
+     * @Orm\Column(type="boolean")
+     */
+    protected $se_entrego_certificado;
+    
+    /**
+     * @Orm\Column(type="string")
+     */
+    protected $fecha_entrega_certificado;
+    
     public function setActividad($actividad)
     {
         $this->actividad = $actividad;
@@ -40,5 +94,21 @@ class Participante
     public function setParticipante($participante)
     {
         $this->participante = $participante;
+    }
+    
+    public function setParticipanteRA($data)
+    {
+        foreach ($data as $participante) {
+            $this->act_actividad_participantes_id = $participante['act_actividad_participantes_id'] == 'new_id' ? null : $participante['act_actividad_participantes_id'];
+            $this->cont_moneda_id = $participante['cont_moneda_id'];
+            $this->identificador_participante = $participante['identificador_participante'];
+            $this->sobrenombre = $participante['sobrenombre'];
+            $this->descripcion = $participante['descripcion'];
+            $this->otra_info = $participante['otra_info'];
+            $this->monto_participante = $participante['monto_participante'];
+            $this->se_imprimio_certificado = $participante['se_imprimio_certificado'];
+            $this->se_entrego_certificado = $participante['se_entrego_certificado'];
+            $this->fecha_entrega_certificado = $participante['fecha_entrega_certificado'];
+        }
     }
 }
--- Implementacion/prh/module/Actividad/src/Actividad/Controller/RelacionAyudaController.php
+++ Implementacion/prh/module/Actividad/src/Actividad/Controller/RelacionAyudaController.php
@@ -4,6 +4,8 @@ namespace Actividad\Controller;
 
 use Actividad\RelacionAyuda\QueryObject\Get;
 use Actividad\RelacionAyuda\QueryObject\Select;
+use Actividad\RelacionAyuda\Service\Crear as CrearRelacionAyudaService;
+use Doctrine\ORM\EntityManager;
 use EnterpriseSolutions\Controller\BaseController;
 use EnterpriseSolutions\Db\Dao;
 use EnterpriseSolutions\Db\Dao\Get as DaoGet;
@@ -24,5 +26,37 @@ class RelacionAyudaController extends BaseController
         $dao = new DaoGet($query);
         $template = $this->_crearTemplateParaGet();
         return $template($dao, array());
+    }
+    
+    public function postAction()
+    {
+        $em = $this->getEntityManager();
+        $data = $this->SubmitParams()->getParam('post');
+        
+        $service = new CrearRelacionAyudaService($em);
+        $service->ejecutar($data);
+        $this->getEntityManager()->flush();
+        
+        return $this->toJson($service->getRespuesta());
+    }
+    
+    /**
+     * Doctrine Entity Manager
+     * @return EntityManager
+     */
+    protected function getEntityManager()
+    {
+        $em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+        return $em;
+    }
+    
+    protected function toJson($respuesta)
+    {
+        $viewModel = $this->_seleccionarViewModelSegunContexto(
+            array('Zend\View\Model\JsonModel' => array('text/html', 'application/json'))
+        );
+        
+        $viewModel->setVariables($respuesta);
+        return $viewModel;
     }
 }
