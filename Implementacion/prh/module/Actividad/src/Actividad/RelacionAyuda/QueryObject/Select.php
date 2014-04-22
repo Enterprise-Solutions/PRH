055d23f1f8dc79f3f1cd8381fb5eda46d8446871
guido <guido@guido>
2014-04-22 10:58:22 -0400

    Actualizacion grilla de relacion de ayuda

--- Implementacion/prh/module/Actividad/src/Actividad/RelacionAyuda/QueryObject/Select.php
+++ Implementacion/prh/module/Actividad/src/Actividad/RelacionAyuda/QueryObject/Select.php
@@ -3,6 +3,7 @@
 namespace Actividad\RelacionAyuda\QueryObject;
 
 use EnterpriseSolutions\Db\Select as DbSelect;
+use Zend\Db\Sql\Select as ZfSelect;
 
 class Select extends DbSelect
 {
@@ -16,6 +17,10 @@ class Select extends DbSelect
              ->join(array('act' => 'act_actividad_tipo'), 'aa.act_actividad_tipo_id = act.act_actividad_tipo_id', array('actividad_tipo' => 'titulo', 'modalidad'))
              ->join(array('cm' => 'cont_moneda'), 'aa.cont_moneda_id = cm.cont_moneda_id', array('moneda' => 'simbolo'))
              ->join(array('caf' => 'cal_anho_formacion'), 'aa.cal_anho_formacion_id = caf.cal_anho_formacion_id', array('anho_formacion' => 'anho'))
+             
+             // Participante
+             ->join(array('aap' => 'act_actividad_participantes'), 'aa.act_actividad_id = aap.act_actividad_id', array(), ZfSelect::JOIN_LEFT)
+             ->join(array('apa' => 'act_participante_anonimo'), 'aap.act_participante_anonimo_id = apa.act_participante_anonimo_id', array('identificador', 'alias'))
              
              ->where("act.relacion_ayuda = 'S'");
     }
@@ -33,6 +38,30 @@ class Select extends DbSelect
         if ($titulo && $titulo != "") {
             $this->_select
                  ->where("aa.titulo ILIKE '%$titulo%'");
+        }
+    }
+    
+    public function addSearchByAlias($alias)
+    {
+        if ($alias && $alias != "") {
+            $this->_select
+                 ->where("apa.alias ILIKE '%$alias%'");
+        }
+    }
+    
+    public function addSearchByIdentificador($identificador)
+    {
+        if ($identificador && $identificador != "") {
+            $this->_select
+                 ->where("apa.identificador ILIKE '%$identificador%'");
+        }
+    }
+    
+    public function addSearchByFecha($fecha)
+    {
+        if ($fecha && $fecha != "") {
+            $this->_select
+                 ->where("aa.fecha_inicio = '$fecha'");
         }
     }
 }
